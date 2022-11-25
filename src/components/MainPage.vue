305666a7b7b2c41498308d7af558561aa98bb0f3
ycycyc02 <2715625283@qq.com>
2022-11-25 15:57:40 +0800

    version1

---
+++ src/components/MainPage.vue
@@ -0,0 +1,111 @@
+<template>
+  <a-layout style="min-height: 100vh">
+    <a-layout-sider v-model:collapsed="collapsed" collapsible>
+      <div class="logo" />
+      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+        <a-menu-item key="1">
+          <pie-chart-outlined />
+          <span>Option 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <desktop-outlined />
+          <span>Option 2</span>
+        </a-menu-item>
+        <a-sub-menu key="sub1">
+          <template #title>
+            <span>
+              <user-outlined />
+              <span>User</span>
+            </span>
+          </template>
+          <a-menu-item key="3">Tom</a-menu-item>
+          <a-menu-item key="4">Bill</a-menu-item>
+          <a-menu-item key="5">Alex</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
+            <span>
+              <team-outlined />
+              <span>Team</span>
+            </span>
+          </template>
+          <a-menu-item key="6">Team 1</a-menu-item>
+          <a-menu-item key="8">Team 2</a-menu-item>
+        </a-sub-menu>
+        <a-menu-item key="9">
+          <file-outlined />
+          <span>File</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <div style="
+            width: 600px;
+            margin: 0 auto;
+            padding-top: 20px;
+        ">
+          <StepCompontent/>
+        </div>
+      </a-layout-header>
+      <a-layout-content>
+        <div :style="{ padding: '24px', background: '#fff', minHeight: '510px' }">
+          <div style="width: 20%; float:left">
+            <UpLoad/>
+          </div>
+          <div style="width: 80%; float:left">
+            <a-table
+              class="table"
+              bordered
+              row-key="key"
+              :columns="uploadColumns"
+              :data-source="uploadData"
+              :pagination="false"
+              :file-list="fileList"
+            >
+            </a-table>
+          </div>
+        </div>
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+import { PieChartOutlined, DesktopOutlined, UserOutlined, TeamOutlined, FileOutlined } from '@ant-design/icons-vue';
+import { defineComponent, ref } from 'vue';
+import StepCompontent from './StepCompontent.vue'
+import UpLoad from './UpLoad.vue'
+// import { Form, Input, InputNumber, Popconfirm, Table, Typography } from 'antd';
+// import React, { useState } from 'react';
+export default defineComponent({
+  components: {
+    PieChartOutlined,
+    DesktopOutlined,
+    UserOutlined,
+    TeamOutlined,
+    FileOutlined,
+    StepCompontent,
+    UpLoad
+  },
+  data() {
+    return {
+      collapsed: ref(false),
+      selectedKeys: ref(['1']),
+    };
+  },
+});
+</script>
+<style>
+#components-layout-demo-side .logo {
+  height: 32px;
+  margin: 16px;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.site-layout .site-layout-background {
+  background: #fff;
+}
+[data-theme='dark'] .site-layout .site-layout-background {
+  background: #141414;
+}
+</style>
---
+++ src/components/StepCompontent.vue
@@ -0,0 +1,8 @@
+<template>
+    <a-steps :current="0" size="small" >
+    <a-step title="上传excel" />
+    <a-step title="修改内容" />
+    <a-step title="知识库查询" />
+    <a-step title="数据集修改" />
+  </a-steps>
+</template>
